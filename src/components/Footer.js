9f27a9fa1aaf745f8e71c71c617ef4b51a2a157c
Brunna Serafina <brunnaserafina@gmail.com>
2022-08-05 14:23:41 -0300

    feat (footer): buttons direct to routes and added circular-progress-bar component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,70 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { useContext } from 'react';
+import UserContext from '../context/UserContext';
+
 export default function Footer() {
-  return '';
+  const { percentage } = useContext(UserContext);
+
+  return (
+    <FooterContainer>
+      <Link to={`/habitos`}>
+        <p>Hábitos</p>
+      </Link>
+
+      <span>
+        <CircularProgressbar
+          value={percentage}
+          text="Hoje"
+          background
+          backgroundPadding={6}
+          styles={buildStyles({
+            backgroundColor: '#52B6FF',
+            textColor: '#ffffff',
+            pathColor: '#ffffff',
+            trailColor: 'transparent',
+          })}
+        />
+      </span>
+
+      <Link to={`/historico`}>
+        <p>Histórico</p>
+      </Link>
+    </FooterContainer>
+  );
 }
+
+const FooterContainer = styled.div`
+  background-color: #ffffff;
+  width: 100vw;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 35px;
+  box-sizing: border-box;
+  position: fixed;
+  bottom: 0;
+
+  a {
+    text-decoration: none;
+    color: #52b6ff;
+    font-size: 18px;
+  }
+
+  span {
+    width: 91px;
+    height: 91px;
+    background-color: #52b6ff;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #ffffff;
+    font-size: 18px;
+    border-radius: 60px;
+    margin-bottom: 30px;
+  }
+`;
