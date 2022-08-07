21430b5a1dc378233eb4ac191098092679e39051
Brunna Serafina <brunnaserafina@gmail.com>
2022-08-07 16:30:02 -0300

    style (PageHistory): text according to layout

--- src/components/PageHistory.js
+++ src/components/PageHistory.js
@@ -3,14 +3,14 @@ import Footer from './Footer';
 import styled from 'styled-components';
 
 export default function History() {
-  
   return (
     <>
+      <Header />
       <HistoryContainer>
-        <Header />
-
-        <Footer />
+        <h1>Histórico</h1>
+        <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
       </HistoryContainer>
+      <Footer />
     </>
   );
 }
@@ -18,4 +18,18 @@ export default function History() {
 const HistoryContainer = styled.div`
   background-color: #e5e5e5;
   height: 100vh;
+  margin-top: 70px;
+  padding: 30px 15px;
+
+  h1 {
+    font-size: 23px;
+    color: #126BA5;
+  }
+
+  p {
+    font-size: 18px;
+    margin-top: 17px;
+    color: #666666;
+    line-height: 22.5px;
+  }
 `;
